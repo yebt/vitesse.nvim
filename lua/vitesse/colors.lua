c9d188606ce503c904697a463ee77bddd4769790
2nthony <ant.hu@outlook.com>
2023-01-08 13:46:06 +0800

    refactor: extract colors

---
+++ lua/vitesse/colors.lua
@@ -0,0 +1,61 @@
+local M = {}
+
+M.colors = {
+  black = "#111111",
+  black1 = "#252525",
+  black2 = "#393939",
+  black3 = "#4d4d4d",
+  white3 = "#c3c3c3",
+  white2 = "#d7d7d7",
+  white1 = "#ebebeb",
+  white = "#ffffff",
+  purple6 = "#5a32a3",
+}
+
+-- dark
+M.themes = {
+  primary = "#4d9375",
+
+  foreground = "#c8c5b8",
+  activeForeground = "#bebaab",
+  secondaryForeground = "#83827d",
+  ignored = "#383837",
+  border = "#191919",
+
+  background = "#121212",
+  activeBackground = "#181818",
+
+  lowBackground = "#222222",
+  lowActiveBackground = "#292929",
+  lowBorder = "#252525",
+
+  comment = "#5c6b5e",
+  string = "#c98a7d",
+  variable = "#bd976a",
+  keyword = "#4d9375",
+  number = "#4C9A91",
+  boolean = "#4d9375",
+  operator = "#cb7676",
+  func = "#80a665",
+  constant = "#c99076",
+  class = "#6893BF",
+  interface = "#5DA994",
+  type = "#5da9a7",
+  builtin = "#cb7676",
+  property = "#b8a965",
+  namespace = "#db889a",
+  punctuation = "#666666",
+  decorator = "#bd8f8f",
+  regex = "#c4704f",
+
+  -- colors
+  green = "#4d9375",
+  cyan = "#5eaab5",
+  blue = "#6394bf",
+  red = "#cb7676",
+  orange = "#d4976c",
+  yellow = "#e6cc77",
+  magenta = "#d9739f",
+}
+
+return M
