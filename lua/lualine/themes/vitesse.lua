8083fcf8a75afcd21a00bf51f72c3a920739bbfd
2nthony <ant.hu@outlook.com>
2023-01-17 16:25:34 +0800

    feat: lualine b foreground color

--- lua/lualine/themes/vitesse.lua
+++ lua/lualine/themes/vitesse.lua
@@ -4,28 +4,33 @@ local vitesse = {}
 
 vitesse.normal = {
   a = { bg = vitesse_theme.primary, fg = vitesse_theme.activeBackground },
-  b = { bg = vitesse_theme.lowActiveBackground, fg = vitesse_theme.foreground },
+  b = { bg = vitesse_theme.lowActiveBackground, fg = vitesse_theme.primary },
   c = { bg = vitesse_theme.activeBackground, fg = vitesse_theme.activeForeground },
 }
 
 vitesse.insert = {
-  a = { bg = vitesse_theme.variable, fg = vitesse_theme.activeBackground }
+  a = { bg = vitesse_theme.variable, fg = vitesse_theme.activeBackground },
+  b = { bg = vitesse_theme.lowActiveBackground, fg = vitesse_theme.variable },
 }
 
 vitesse.command = {
-  a = { bg = vitesse_theme.yellow, fg = vitesse_theme.activeBackground }
+  a = { bg = vitesse_theme.yellow, fg = vitesse_theme.activeBackground },
+  b = { bg = vitesse_theme.lowActiveBackground, fg = vitesse_theme.yellow },
 }
 
 vitesse.visual = {
-  a = { bg = vitesse_theme.magenta, fg = vitesse_theme.activeBackground }
+  a = { bg = vitesse_theme.magenta, fg = vitesse_theme.activeBackground },
+  b = { bg = vitesse_theme.lowActiveBackground, fg = vitesse_theme.magenta },
 }
 
 vitesse.replace = {
-  a = { bg = vitesse_theme.red, fg = vitesse_theme.activeBackground }
+  a = { bg = vitesse_theme.red, fg = vitesse_theme.activeBackground },
+  b = { bg = vitesse_theme.lowActiveBackground, fg = vitesse_theme.red },
 }
 
 vitesse.terminal = {
-  a = { bg = vitesse_theme.blue, fg = vitesse_theme.activeBackground }
+  a = { bg = vitesse_theme.blue, fg = vitesse_theme.activeBackground },
+  b = { bg = vitesse_theme.lowActiveBackground, fg = vitesse_theme.blue },
 }
 
 vitesse.inactive = {
