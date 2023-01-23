f1e36ea10fa5befe55e028ed47a3a0f35010f527
2nthony <ant.hu@outlook.com>
2023-01-23 15:45:54 +0800

    fix(lspsaga): highlight

--- lua/vitesse/plugins/lspsaga.lua
+++ lua/vitesse/plugins/lspsaga.lua
@@ -3,24 +3,11 @@ local Group = require("colorbuddy.init").Group
 local groups = require("colorbuddy.init").groups
 local styles = require("colorbuddy.init").styles
 
--- old
-Group.new("LspSagaCodeActionTitle", colors.green)
-Group.new("LspSagaBorderTitle", colors.yellow, colors.none, styles.bold)
-Group.new("LspSagaDiagnosticHeader", colors.yellow)
-Group.new("LspSagaShTruncateLine", colors.black1)
-Group.new("LspSagaDocTruncateLine", colors.black1)
-Group.new("LspSagaCodeActionTruncateLine", colors.black1)
-Group.new("LspSagaHoverBorder", groups.FloatBorder)
-Group.new("LspSagaRenameBorder", groups.LspSagaHoverBorder)
-Group.new("LSPSagaDiagnosticBorder", groups.LspSagaHoverBorder)
-Group.new("LspSagaSignatureHelpBorder", groups.LspSagaHoverBorder)
-Group.new("LspSagaCodeActionBorder", groups.LspSagaHoverBorder)
-Group.new("LspSagaLspFinderBorder", groups.LspSagaHoverBorder)
-Group.new("LspSagaFloatWinBorder", groups.LspSagaHoverBorder)
-Group.new("LspSagaSignatureHelpBorder", groups.LspSagaHoverBorder)
-Group.new("LspSagaDefPreviewBorder", groups.LspSagaHoverBorder)
-Group.new("LspSagaAutoPreviewBorder", groups.LspSagaHoverBorder)
-Group.new("LspSagaFinderSelection", colors.green, colors.none, styles.bold)
-
-Group.new("LspFloatWinBorder", groups.LspSagaHoverBorder)
-Group.new("LspLinesDiagBorder", groups.LspSagaHoverBorder)
+Group.new("SagaBorder", colors.primary)
+Group.new("CodeActionText", colors.yellow)
+Group.new("FinderSelection", colors.primary, colors.none, styles.bold)
+Group.new("RenameNormal", colors.variable)
+Group.link("DiagnosticErrorBorder", groups.Error)
+Group.link("DiagnosticWarnBorder", groups.Warning)
+Group.link("DiagnosticHintBorder", groups.Hint)
+Group.link("DiagnosticInfoBorder", groups.Information)
