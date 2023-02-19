d969888666729cc30c74c8114c20e441c3c50b47
2nthony <ant.hu@outlook.com>
2023-02-19 18:29:37 +0800

    fix: which key highlight

--- lua/vitesse/plugins/which-key.lua
+++ lua/vitesse/plugins/which-key.lua
@@ -1,7 +1,5 @@
 local colors = require("colorbuddy.init").colors
 local Group = require("colorbuddy.init").Group
-local styles = require("colorbuddy.init").styles
 
-
-Group.new("WhichKeySeparator", colors.activeBackground, colors.black1, styles.bold)
-Group.new("WhichKeyDesc", colors.cyan, colors.black1)
+Group.new("WhichKeySeparator", colors.secondaryForeground, colors.none)
+Group.new("WhichKeyDesc", colors.cyan, colors.none)
