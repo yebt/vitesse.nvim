8b4c6001dba37f53dec63ce6c03a67856899fe59
2nthony <ant.hu@outlook.com>
2023-02-23 02:27:54 +0800

    feat: lspsaga, winbar seperator color

--- lua/vitesse/plugins/lspsaga.lua
+++ lua/vitesse/plugins/lspsaga.lua
@@ -3,7 +3,7 @@ local Group = require("colorbuddy.init").Group
 local groups = require("colorbuddy.init").groups
 local styles = require("colorbuddy.init").styles
 
-Group.new("SagaBorder", colors.primary)
+Group.new("SagaBorder", groups.FloatBorder)
 Group.new("CodeActionText", colors.yellow)
 Group.new("FinderSelection", colors.primary, colors.none, styles.bold)
 Group.new("RenameNormal", colors.variable)
@@ -11,3 +11,4 @@ Group.link("DiagnosticErrorBorder", groups.Error)
 Group.link("DiagnosticWarnBorder", groups.Warning)
 Group.link("DiagnosticHintBorder", groups.Hint)
 Group.link("DiagnosticInfoBorder", groups.Information)
+Group.link("SagaWinbarSep", groups.Normal)
