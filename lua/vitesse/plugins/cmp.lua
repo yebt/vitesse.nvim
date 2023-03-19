642e717ac09060fb7775fc86d204f80d87d9a45f
2nthony <ant.hu@outlook.com>
2023-03-19 13:05:19 +0800

    cmp, codeium

--- lua/vitesse/plugins/cmp.lua
+++ lua/vitesse/plugins/cmp.lua
@@ -1,15 +1,18 @@
+local Color = require("colorbuddy.init").Color
 local colors = require("colorbuddy.init").colors
 local Group = require("colorbuddy.init").Group
 local groups = require("colorbuddy.init").groups
 local styles = require("colorbuddy.init").styles
-local link = require('vitesse.utils').highlight_link
+local link = require("vitesse.utils").highlight_link
+
+Color.new("codeium", "#09B6A2")
 
 Group.new("CmpItemKind", colors.green)
 Group.new("CmpItemMenu", colors.secondaryForeground)
 -- Group.new('CmpItemAbbr', colors.secondaryForeground, colors.none, styles.none)
 -- Group.new('CmpItemAbbrMatch', colors.secondaryForeground, colors.none, styles.none)
-Group.new('CmpItemAbbrMatch', colors.blue, colors.none, styles.none)
-Group.new('CmpItemAbbrMatchFuzzy', groups.CmpItemAbbrMatch)
+Group.new("CmpItemAbbrMatch", colors.blue, colors.none, styles.none)
+Group.new("CmpItemAbbrMatchFuzzy", groups.CmpItemAbbrMatch)
 Group.new("CmpItemAbbrDeprecated", colors.secondaryForeground, colors.none, styles.strikethrough)
 Group.new("CmpItemKindModule", colors.green, colors.none, styles.none)
 Group.new("CmpItemKindUnit", colors.orange, colors.none, styles.none)
@@ -21,19 +24,20 @@ Group.new("CmpItemKindFile", colors.purple6, colors.none, styles.none)
 Group.new("CmpItemKindFolder", colors.purple6, colors.none, styles.none)
 Group.new("CmpItemKindStruct", colors.yellow, colors.none, styles.none)
 Group.new("CmpItemKindEvent", colors.orange, colors.none, styles.none)
+Group.new("CmpItemKindCodeium", colors.codeium, colors.none, styles.none)
 
-link('CmpItemKindText', '@text')
-link('CmpItemKindMethod', '@method')
-link('CmpItemKindFunction', '@function')
-link("CmpItemKindConstructor", '@constructor')
-link("CmpItemKindField", '@field')
-link("CmpItemKindVariable", '@variable')
-link("CmpitemKindClass", '@class')
-link("CmpItemKindInterface", '@interface')
-link("CmpItemKindProperty", '@property')
-link("CmpItemKindKeyword", '@keyword')
-link("CmpItemKindReference", '@text.reference')
-link("CmpItemKindEnumMember", '@type')
-link("CmpItemKindConstant", '@constant')
-link("CmpItemKindOperator", '@operator')
-link("CmpItemKindTypeParameter", '@type')
+link("CmpItemKindText", "@text")
+link("CmpItemKindMethod", "@method")
+link("CmpItemKindFunction", "@function")
+link("CmpItemKindConstructor", "@constructor")
+link("CmpItemKindField", "@field")
+link("CmpItemKindVariable", "@variable")
+link("CmpitemKindClass", "@class")
+link("CmpItemKindInterface", "@interface")
+link("CmpItemKindProperty", "@property")
+link("CmpItemKindKeyword", "@keyword")
+link("CmpItemKindReference", "@text.reference")
+link("CmpItemKindEnumMember", "@type")
+link("CmpItemKindConstant", "@constant")
+link("CmpItemKindOperator", "@operator")
+link("CmpItemKindTypeParameter", "@type")
