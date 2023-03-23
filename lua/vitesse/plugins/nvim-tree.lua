9da8573e7790a25ff62411c7f4c5218df305aff2
2nthony <ant.hu@outlook.com>
2023-03-23 22:26:29 +0800

    nvim tree folder icon color

--- lua/vitesse/plugins/nvim-tree.lua
+++ lua/vitesse/plugins/nvim-tree.lua
@@ -1,8 +1,9 @@
 local colors = require("colorbuddy.init").colors
 local Group = require("colorbuddy.init").Group
+local groups = require("colorbuddy.init").groups
 local styles = require("colorbuddy.init").styles
 
-Group.new("NvimTreeFolderIcon", colors.blue)
-Group.new("NvimTreeRootFolder", colors.orange)
+Group.new("NvimTreeFolderIcon", groups.Folder)
+Group.new("NvimTreeRootFolder", groups.FolderRoot)
 Group.new("NvimTreeImageFile", colors.orange)
 Group.new("NvimTreeSpecialFile", colors.orange, colors.none, styles.bold + styles.underline)
